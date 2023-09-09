fcf98c514df65ec3c6448fd00db778eed0bad6b9
kimdev <kimdev1242@gmail.com>
2023-09-09 09:16:27 +0900

    스프링부트 프로젝트 시
    스프링 부트 스타

---
+++ project-board/src/main/java/com/practice/project/ProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.practice.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBoardApplication.class, args);
+	}
+
+}
