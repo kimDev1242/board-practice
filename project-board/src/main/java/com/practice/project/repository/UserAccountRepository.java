7e9424c3b647afb14b9eaa33e0e77aa50b8c0576
kimdev <kimdev1242@gmail.com>
2023-10-14 11:36:30 +0900

    Data REST API 설정
    Data REST API 설정

--- project-board/src/main/java/com/practice/project/repository/UserAccountRepository.java
+++ project-board/src/main/java/com/practice/project/repository/UserAccountRepository.java
@@ -1,7 +1,9 @@
 package com.practice.project.repository;
 
 import com.practice.project.domain.UserAccount;
+import com.practice.project.domain.projection.UserAccountProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(excerptProjection = UserAccountProjection.class)
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
